06a2d00bf15ee65ba488eb7aaa92d0cc3fd324af
Brett Baker <brett.baker@bronto.com>
2016-02-01 13:10:16 -0500

    added working version of script from bronto.com

---
+++ corpsite-inpage-redirect-to-a-file.js
@@ -0,0 +1,19 @@
+// remember to change the URL, GA Source, GA Medium, GA Content, and GA Campaign
+
+<script>
+var $j = jQuery.noConflict();
+$j(document).ready(function() {
+
+setTimeout(function() {
+
+ ga('send','event', 'redirect', 'auto', '2016Trends', 1);
+ window.location = "http://mkto.bronto.com/rs/797-YXJ-932/images/WP_2016_Trends_Strategies_v1.pdf?utm_source=bronto&utm_medium=email&utm_content=2016Trends&utm_campaign=CM_Welcome";
+});
+
+    }, 5000);
+</script>
+<style type="text/css">
+#region-page-bottom, 
+#page {
+display:none;
+}</style>
